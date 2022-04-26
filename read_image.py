33c557c7a3e74131ccbd3a82a8f0a8bc111b9b4a
SamDaQueen <samreenreyaz@outlook.com>
2022-04-26 00:51:00 -0400

    Merge LAB into BGR

--- read_image.py
+++ read_image.py
@@ -1,7 +1,8 @@
 import sys
-import numpy as np
+
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 from PIL import Image
 from torchvision.transforms import RandomResizedCrop
 
@@ -38,6 +39,22 @@ def plot_LAB(image, lab_image):
     cv2.imwrite('./B.png', B)
 
 
+def combine(L, A, B):
+    # combine the LAB images
+    lab_image = cv2.merge((L, A, B))
+    # convert back to RGB
+    return cv2.cvtColor(lab_image, cv2.COLOR_LAB2BGR)
+
+
+def plot_predicted_image(original_image, predicted_image):
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(original_image)
+    ax[0].set_title('Original')
+    ax[1].imshow(predicted_image)
+    ax[1].set_title('Predicted')
+    plt.show()
+
+
 def main(argv):
     if len(argv) != 2:
         print("Usage: python read_image.py <image_file>")
@@ -51,7 +68,12 @@ def main(argv):
     randresizecrop = RandomResizedCrop(128)
     image = randresizecrop(image)
     lab_image = convertToLAB(image)
-    plot_LAB(image, lab_image)
+    merged_image = combine(
+        lab_image[:, :, 0], lab_image[:, :, 1], lab_image[:, :, 2])
+    # plot_LAB(image, lab_image, merged_image)
+    # plot the original and predicted images
+    plot_predicted_image(image, merged_image)
+
     return 0
 
 
