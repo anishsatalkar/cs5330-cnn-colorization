63de69f00a191e048138355b5ed39036db58634b
Anish Satalkar <anishsatalkar@gmail.com>
2022-05-04 13:00:47 -0400

    Removed TODOs

--- model.py
+++ model.py
@@ -11,26 +11,21 @@ class GrayscaleToColorModel(nn.Module):
     def __init__(self, size=128):
         super(GrayscaleToColorModel, self).__init__()
 
-        # TODO: add num_classes argument
         resnet = models.resnet18(num_classes=365)
 
         # Change the weight of the first layer so that it accepts single channel grayscale input.
         resnet.conv1.weight = nn.Parameter(
             resnet.conv1.weight.sum(dim=1).unsqueeze(1))
 
-        # TODO: Check resnet.children(), does increasing the layers improve the accuracy?
         # Use only the first 6 layers of ResNet18
         self.resnet_layers = nn.Sequential(*list(resnet.children())[0:6])
 
-        # TODO: Why to upsample?
         # Upsample the output from the last layer of ResNet
         self.upsample_layers = nn.Sequential(
-            # TODO: Try padding 1.
             nn.Conv2d(size, 128, kernel_size=3, padding=1),
-            # TODO
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            # TODO Try varying the scale_factor
+
             nn.Upsample(scale_factor=2),
             nn.Conv2d(128, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
