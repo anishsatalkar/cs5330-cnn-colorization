9ac3f5c23ddc7c12d4f9187d580b63735113f870
Anish Satalkar <anishsatalkar@gmail.com>
2022-05-04 12:59:24 -0400

    Added code documentation in config_reader.py.

--- config_reader.py
+++ config_reader.py
@@ -1,8 +1,13 @@
 import json
 
+
 class ConfigReader(object):
     @staticmethod
     def read():
+        """
+        Reads a JSON file that contains the config required for the project.
+        :return: Dictionary that contains the config.
+        """
         with open('config.json') as config_file:
             config = json.load(config_file)
             return config
